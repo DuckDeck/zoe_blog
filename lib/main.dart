0ad95babd72404bb4f23135818eae47382452c86
“stanhu” <3421902@qq.com>
2019-01-02 17:53:44 +0800

    first page ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:zoe_blog/model/article.dart';
+import 'package:zoe_blog/pages/tab_page.dart';
+import 'package:redux/redux.dart';
+import 'package:zoe_blog/redux/states/reduxState.dart';
+import 'package:zoe_blog/redux/view_modules/viewModel.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final Store<ReduxState> store;
+  MyApp({this.store});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return StoreProvider<ReduxState>(
+      store: store,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.blue,
+          textTheme: TextTheme(
+            body1: TextStyle(fontSize: 15.0,color: Colors.black87),
+            body2: TextStyle(fontSize: 13.0,color: Colors.grey),
+            caption: TextStyle(fontSize: 10.0,color: Colors.grey),
+            display1: TextStyle(fontSize: 14.0,color: Colors.black87,fontWeight: FontWeight.bold)
+          ),
+          
+        ),
+        home: TabPage(),
       ),
-    
     );
   }
 }
