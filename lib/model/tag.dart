73f419fffc843710696cd4a9dc02d9650cfcfaf3
“stanhu” <3421902@qq.com>
2019-01-02 09:45:57 +0800

    process code

--- lib/model/tag.dart
+++ lib/model/tag.dart
@@ -1,4 +1,8 @@
 class Tag{
   int tagId;
   String tagName; 
+  Tag.fromJson(Map<String, dynamic> json) {
+    tagId = json['tag_id'];
+    tagName = json['tag_name'];
+  }
 }
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,5 +1,10 @@
 class User{
   int userId;
   int userImage;
-  String name;
+  String readName;
+  User.fromJson(Map<String, dynamic> json) {
+    userId = json['user_id'];
+    userImage = json['user_image_url'];
+    readName = json['user_real_name'];
+  }
 }
