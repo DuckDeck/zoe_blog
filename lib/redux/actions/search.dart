18544831bd28cdb1d7fe9b74ec93d92952b2cfe4
“stanhu” <3421902@qq.com>
2019-01-08 17:22:19 +0800

    process code

---
+++ lib/redux/actions/search.dart
@@ -0,0 +1,3 @@
+import 'package:zoe_blog/model/article.dart';
+import 'package:zoe_blog/redux/actions/main.dart';
+
--- lib/redux/states/reduxState.dart
+++ lib/redux/states/reduxState.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:zoe_blog/redux/states/home.dart';
+import 'package:zoe_blog/redux/states/search.dart';
 @immutable
 class ReduxState{
   final HomeState home;
-
-  const ReduxState({this.home});
+  final SearchStates search;
+  const ReduxState({this.home,this.search});
 }
