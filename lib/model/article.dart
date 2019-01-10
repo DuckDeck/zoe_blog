c66c483174c7805b38d1e06efe1fa2bbac423f37
“stanhu” <3421902@qq.com>
2019-01-10 14:07:34 +0800

    process code

--- lib/model/article.dart
+++ lib/model/article.dart
@@ -116,16 +116,6 @@ class Article{
   
 
 
-  static searchSort(String key,int index,int size) async{
-    final url = "http://api.bqbbq.com/api/search/$key/article/$index/$size";
-    final httpClient = Dio();
-    final res = await httpClient.get(url);
-    var map = res.data as Map<String,dynamic>;
-    if(map["code"] != 0){
-      // report error
-      return;
-    }
-    print(map["data"]);
-  }
+  
 
 }
--- lib/model/sort.dart
+++ lib/model/sort.dart
@@ -1,4 +1,14 @@
 import 'package:dio/dio.dart';
 class Sort{
-
+  static searchSort(String key,int index,int size) async{
+    final url = "http://api.bqbbq.com/api/search/$key/article/$index/$size";
+    final httpClient = Dio();
+    final res = await httpClient.get(url);
+    var map = res.data as Map<String,dynamic>;
+    if(map["code"] != 0){
+      // report error
+      return;
+    }
+    print(map["data"]);
+  }
 }
